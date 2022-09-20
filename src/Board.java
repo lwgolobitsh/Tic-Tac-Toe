4e629d0dd04903f8c78812bf9a31fbc32d1262ab
lwgolobitsh <lwgolobitsh@stu.naperville203.org>
2022-09-20 13:29:24 -0500

    ;lkj

--- src/Board.java
+++ src/Board.java
@@ -1,10 +1,20 @@
 package src;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class Board extends JFrame{
+public class Board extends JPanel{
+
+    private int w;
+    private int h;
     public Board(int w, int h) {
         this.setSize(w, h);
         this.setVisible(true);
+
+    }
+
+    public void paint(Graphics g) {  
+        g.setColor(Color.RED);
+        g.drawLine(0, 0, w, h);
     }
 }
