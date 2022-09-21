cafd424055843cb7d44cf56ea653b3692a3d76cd
lwgolobitsh <lwgolobitsh@stu.naperville203.org>
2022-09-21 10:38:29 -0500

    commit

--- src/Box.java
+++ src/Box.java
@@ -1,14 +1,62 @@
 package src;
 
 import javax.swing.*;
-import java.awt.event.*;
+import javax.swing.event.MouseInputListener;
+import java.awt.event.MouseEvent;
 
 public class Box extends JComponent{
-    public static void main(String[] args) {
-        
+
+    String val = null;
+
+    public Box() {
+        this.addMouseListener(new ClickListener());
     }
 
     private class ClickListener implements MouseInputListener {
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            // TODO Auto-generated method stub
+            val = "X";
+            System.out.println("x");
+            
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            // TODO Auto-generated method stub
+            
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+            // TODO Auto-generated method stub
+            
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+            // TODO Auto-generated method stub
+            
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            // TODO Auto-generated method stub
+            
+        }
+
+        @Override
+        public void mouseDragged(MouseEvent e) {
+            // TODO Auto-generated method stub
+            
+        }
+
+        @Override
+        public void mouseMoved(MouseEvent e) {
+            // TODO Auto-generated method stub
+            
+        }
         
     }
 }
